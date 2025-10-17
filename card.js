843f0a4823bb69a4b17b26446260bf90bf18ab12
OhMyMoh <moyinp02@gmail.com>
2025-10-17 11:53:22 +0100

    HNG-done

---
+++ card.js
@@ -0,0 +1,17 @@
+const timeElement = document.getElementById("userTime");
+function updateTime() {
+  timeElement.textContent = Date.now();
+}
+setInterval(updateTime, 1000);
+updateTime();
+
+const avatarUpload = document.getElementById("avatarUpload");
+const avatarImage = document.getElementById("avatarImage");
+
+avatarUpload.addEventListener("change", (e) => {
+  const file = e.target.files[0];
+  if (file) {
+    const imageURL = URL.createObjectURL(file);
+    avatarImage.src = imageURL;
+  }
+});
